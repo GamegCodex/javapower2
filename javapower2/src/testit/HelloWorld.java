f802cdeb2c9b5edc379fe72079d9509c0dcd39dd
Marko <Marko@Mamagameg.53kdhm0lxs0uldwzrppbrovzph.cwx.internal.cloudapp.net>
2019-06-20 10:38:17 +0300

    Tokatesti

---
+++ javapower2/src/testit/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testit;
+
+/**
+ * @author Marko Holmström (marko.holmstrom@gameg.fi) 
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Main code
+
+	}
+
+}
