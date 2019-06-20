e7cd5eb49b01fbfbd49d06110c4a1b413553ed15
Marko <Marko@Mamagameg.53kdhm0lxs0uldwzrppbrovzph.cwx.internal.cloudapp.net>
2019-06-20 10:37:11 +0300

    eka commit

---
+++ javapower2/src/testit/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testit;
+
+/**
+ * @author Marko Holmström (marko.holmstrom@gameg.fi) 
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Main code
+		System.out.println("Hello World!");
+
+	}
+
+}
